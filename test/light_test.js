8d32251b28c881cc8c5e46491457bcab1e010553
Max Tedford <max.tedford@gmail.com>
2015-08-31 19:38:24 -0600

    add light tests and functionality

---
+++ test/light_test.js
@@ -0,0 +1,27 @@
+const assert = require('chai').assert;
+const Light = require('../lib/light');
+
+describe('light', function() {
+  
+  it('should exist', function() {
+    assert(Light);
+  });
+  
+  it('is an object', function() {
+    assert(new Light());
+  });
+  
+  it('should default to a status of off', function() {
+    var light = new Light();
+    assert.equal(light.turnedOn, false);
+  });
+  
+  it('be turned on', function() {
+    var light = new Light();
+    assert.equal(light.turnedOn, false);
+    light.changeStatus();
+    assert.equal(light.turnedOn, true);
+    light.changeStatus();
+    assert.equal(light.turnedOn, false);
+  })
+});
