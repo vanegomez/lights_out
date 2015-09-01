17fbcb3024126ecc7ca898f3e628fb086b9cece0
vanegomez <vane.gomez@me.com>
2015-08-31 22:26:59 -0600

    add test row has lights

--- test/board_test.js
+++ test/board_test.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const Board  = require('../lib/board');
+const Light  = require('../lib/light');
 
 describe('board', function() {
   it('should exist', function() {
@@ -13,5 +14,13 @@ describe('board', function() {
   it('has five rows when created', function() {
     var board = new Board();
     assert.equal(board.matrix.length, 5)
+  });
+
+  it('has rows made up of lights', function() {
+    var board = new Board();
+    var row = board.matrix[0];
+    row.forEach(function(light) {
+      assert.instanceOf(light, Light)
+    });
   });
 });
