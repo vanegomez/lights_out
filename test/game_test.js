1d6b26a64ff1daefe95c6915051223e2db1a075c
vanegomez <vane.gomez@me.com>
2015-09-06 22:25:37 -0600

    create test that game is over

--- test/game_test.js
+++ test/game_test.js
@@ -16,8 +16,58 @@ describe('game', function() {
     var targetDiv = $('#test-game');
     game.render(targetDiv);
 
-    var renderedGame = document.querySelector('.board')
+    var renderedGame = document.getElementsByClassName('.board')
     assert.isNotNull(renderedGame);
+  });
+
+  xit('should wipeout', function() {
+    var game = new Game();
+    var targetDiv = $('#test-game');
+    game.render(targetDiv);
+
+    game.wipeOut();
+    var removedClassBoard = document.getElementsByClassName('.board');
+    var removedClassColumn = document.getElementsByClassName('.column');
+    var removedClassLight = document.getElementsByClassName('.light');
+
+    //assert.include(removedClassBoard, false);
+    assert.notInclude(document,'.board');
+    assert.isNotNull(removedClassLight);
+  });
+
+  it("should toggle it's lights", function() {
+    var game = new Game();
+    var targetDiv = $('#test-game');
+    game.render(targetDiv);
+
+    game.toggleLights(0, 0);
+
+    var lights = [];
+    game.board.columns.forEach(function(column) {
+      lights.push(column.lights)
+    });
+
+    var testedLight = _.flatten(lights)[0];
+    var secondTestedLight = _.flatten(lights)[1];
+    var sixthTestedLight = _.flatten(lights)[5];
+
+    assert.equal(testedLight.turnedOn, true);
+    assert.equal(secondTestedLight.turnedOn, true);
+    assert.equal(sixthTestedLight.turnedOn, true);
+
+    game.toggleLights(0, 0);
+
+    assert.equal(testedLight.turnedOn, false);
+    assert.equal(secondTestedLight.turnedOn, false);
+    assert.equal(sixthTestedLight.turnedOn, false);
+  });
+
+  it('checks if game is over by having all lights off', function() {
+    var game = new Game();
+    var targetDiv = $('#test-game');
+    game.render(targetDiv);
+
+    assert.equal(game.allLightsOff(), true)
   });
 });
 
