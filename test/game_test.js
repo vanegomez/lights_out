285292be4d4b5cf9b0ea3a38c50e7625e2339222
vanegomez <vane.gomez@me.com>
2015-09-06 21:24:06 -0600

    test game render

--- test/game_test.js
+++ test/game_test.js
@@ -2,14 +2,22 @@ const assert = require('chai').assert;
 const $      = require('jquery');
 const Game   = require('../lib/game');
 
-//describe('game', function() {
-//  it('should render itself', function() {
-//    var game = new Game();
-//    var targetDiv = $('#test-game');
-//    game.render(targetDiv);
-//
-//    var renderedGame = document.querySelector('.game')
-//
-//    assert.isNotNull(renderedGame);
-//  });
-//});
+describe('game', function() {
+  it('should exist', function() {
+    assert(Game);
+  });
+
+  it('is an object', function() {
+    assert(new Game());
+  });
+
+  it('should render itself', function() {
+    var game = new Game();
+    var targetDiv = $('#test-game');
+    game.render(targetDiv);
+
+    var renderedGame = document.querySelector('.board')
+    assert.isNotNull(renderedGame);
+  });
+});
+
